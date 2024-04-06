9ae0524aeef3892da4d4ed45a0ac0c338119b53d
Mishieck <mishieckmwale@gmail.com>
2024-04-06 08:14:39 +0200

    feat(custom/plugins): add keymap for openning terminal

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -2,6 +2,8 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
+    vim.keymap.set('n', '<leader>$', ':ToggleTerm<CR>', { desc = 'Open terminal' })
+
     require('toggleterm').setup {}
   end,
 }
